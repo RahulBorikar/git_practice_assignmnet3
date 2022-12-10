4dce3853e93ea8447290ddcc5baf4d94dd2dda0c
RahulBorikar <borikarsrahul@gmail.com>
2022-12-10 12:23:12 +0530

    please check the code

---
+++ Prime_Problem.js
@@ -0,0 +1,19 @@
+let number = 43;
+
+function prime(num){
+    let count = 0;
+    for(let i = 2; i < num; i++){
+        if(num % i == 0){
+            count++;
+        } 
+    }
+    if(count === 2){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(prime(number));
+
+
